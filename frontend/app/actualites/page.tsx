3b3a7494bbd2f21a0d509043f4e93a4e1ea2a1ab
Rahajason <lalaina@rahajason.com>
2026-04-28 09:34:51 +0200

    frontend menu

--- frontend/app/actualites/page.tsx
+++ frontend/app/actualites/page.tsx
@@ -1,7 +1,7 @@
 export default function Podcast() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      HOPE RADIO LE PODCAST
+      HOPE RADIO ACTUALITES
     </div>
   );
 }
